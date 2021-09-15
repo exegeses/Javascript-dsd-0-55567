49e2c6602397392c3fa20757f569e07ac7811c16
Marcos <exegeses@gmail.com>
2021-09-15 19:33:37 -0300

    resolucion tarea sumar

--- clase2/js/sumar.js
+++ clase2/js/sumar.js
@@ -5,6 +5,12 @@ var txt = document.getElementById('txt');
 var numero1 = prompt('ingrese un número');
 var numero2 = prompt('ingrese otro número');
 
-var suma = numero1 + numero2;
+//var suma = parseInt (numero1) + parseInt (numero2);
+var suma = parseFloat (numero1) + parseFloat (numero2);
+//numero1 = new Number( numero1 );
+//numero2 = new Number( numero2 );
+//var suma = numero1 + numero2;
 
     txt.innerText = suma;
+
+
