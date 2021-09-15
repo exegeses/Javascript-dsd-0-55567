c997341bbf82ba6a1f7023fa196c5ced1d289cd8
Marcos <exegeses@gmail.com>
2021-09-15 20:13:33 -0300

    teoría funciones

---
+++ clase3/js/funciones.js
@@ -0,0 +1,17 @@
+//ubicamos elemento/s dentro del DOM
+var caja = document.getElementById('caja');
+
+//código no interactivo
+/*
+caja.style.backgroundColor = '#86ee9d';
+caja.style.width = '100px';
+caja.style.height = '150px';
+*/
+
+//declaramos una función
+function cambiarCSS()
+{
+    caja.style.backgroundColor = '#86ee9d';
+    caja.style.width = '100px';
+    caja.style.height = '150px';
+}
