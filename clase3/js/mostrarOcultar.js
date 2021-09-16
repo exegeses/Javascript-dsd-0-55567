da18679b8717cf3ad2b99bffe3b8290b7c018a62
Marcos <exegeses@gmail.com>
2021-09-15 21:24:42 -0300

    práctica funciones - mostrar|ocultar

---
+++ clase3/js/mostrarOcultar.js
@@ -0,0 +1,13 @@
+//ubicar elemento dentro del DOM
+var caja = document.getElementById('caja');
+
+//declaramos funciones de control
+function ocultarCaja()
+{
+    caja.style.display = 'none';
+}
+function mostrarCaja()
+{
+    caja.style.display = 'block';
+}
+
